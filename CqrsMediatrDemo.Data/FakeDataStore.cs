b3f9d4f80e3b48dcb6a9958f285203f84fa12a6b
Diego Pereira <diego.pereira@prosoft.com.br>
2023-07-04 13:01:44 -0300

    Initial commit and project setup

---
+++ CqrsMediatrDemo.Data/FakeDataStore.cs
@@ -0,0 +1,34 @@
+﻿using CqrsMediatrDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CqrsMediatrDemo.Data
+{
+    public class FakeDataStore
+    {
+        private List<Product> _products;
+
+        public FakeDataStore()
+        {
+            _products = new List<Product>()
+            {
+                new Product{ Id = 1, Name = "Test Product 1" },
+                new Product{ Id = 2, Name = "Test Product 2" },
+                new Product{ Id = 3, Name = "Test Product 3" }
+            };
+        }
+
+        public IList<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public void AddAsync(Product product)
+        {
+            _products.Add(product);
+        }
+    }
+}
